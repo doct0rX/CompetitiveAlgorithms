b13d27882426931a99b12028d6fe4a331e4e7a3c
doct0rX <mustafaj4m@gmail.com>
2019-03-16 18:45:42 +0200

    1007. Minimum Domino Rotations For Equal Row

---
+++ LeetCode/Weekly Contest 127/ConstructBinarySearchTreefromPreorderTraversal.java
@@ -0,0 +1,19 @@
+/**
+ * https://leetcode.com/contest/weekly-contest-127/problems/construct-binary-search-tree-from-preorder-traversal/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class ConstructBinarySearchTreefromPreorderTraversal {
+
+    public TreeNode bstFromPreorder(int[] preorder) {
+        
+    }
+}
--- LeetCode/Weekly Contest 127/MinimumDominoRotationsForEqualRow.java
+++ LeetCode/Weekly Contest 127/MinimumDominoRotationsForEqualRow.java
@@ -4,8 +4,40 @@
 
 class MinimumDominoRotationsForEqualRow {
 
-    public int minDominoRotations(int[] A, int[] B) {
+    static class Two{
+        static Two common = null;
+        Integer a, b;
+        Two(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
         
-        return 0;   
+        static void mcommon(Two other) {
+            if(common.a != other.a && common.a != other.b)
+                common.a = null;
+            if(common.b != other.a && common.b != other.b)
+                common.b = null;
+        }
+    }
+
+    public int minDominoRotations(int[] A, int[] B) {
+        Two.common = new Two(A[0], B[0]);
+        for (int i = 1; i < A.length; i++) {
+            Two.mcommon(new Two(A[i], B[i]));
+            if (Two.common.a == null && Two.common.b == null)
+                return -1;
+        }
+        //find the common item in each pair (Two)
+        int c = Two.common.a == null? Two.common.b: Two.common.a;
+        //find its count in both the arrays
+        int a_count=0,b_count=0;
+        for(int i=0; i<A.length; i++){
+            if(A[i] == c)
+                a_count++;
+            if(B[i] == c)
+                b_count++;
+        }
+        //find the number of rotations required
+        return A.length - Math.max(a_count, b_count);
     }
 }
