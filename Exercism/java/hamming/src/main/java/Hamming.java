9fb15295d3983e7fc220ef76758e94e5cdb83226
MustafaJ4m <mustafaj4m@gmail.com>
2020-04-28 15:18:35 +0200

    updating hamming

--- Exercism/java/hamming/src/main/java/Hamming.java
+++ Exercism/java/hamming/src/main/java/Hamming.java
@@ -3,29 +3,21 @@ import java.util.Objects;
 public class Hamming {
     int hammingDistance = 0;
     public Hamming(String leftStrand, String rightStrand) {
-        try {
-            if (leftStrand.length() != rightStrand.length()) {
-                if (leftStrand.equals("")) {
-                    throw new IllegalArgumentException("left strand must not be empty.");
-                }
-                if (rightStrand.equals("")){
-                    throw new IllegalArgumentException("right strand must not be empty.");
-                }
-                throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
-            } else {
-                char[] rightChars = rightStrand.toCharArray();
-                int cCount = 0;
-                for (char c : leftStrand.toCharArray()) {
-                    if (c != rightChars[cCount]) {
-                        hammingDistance += 1;
-                    }
-                    cCount += 1;
+        if (leftStrand.length() != rightStrand.length()) {
+            if (leftStrand.equals("")) {
+                throw new IllegalArgumentException("left strand must not be empty.");
+            }
+            if (rightStrand.equals("")) {
+                throw new IllegalArgumentException("right strand must not be empty.");
+            }
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        } else {
+            for (int i = 0; i < leftStrand.length(); i++) {
+                if (leftStrand.codePointAt(i) != rightStrand.codePointAt(i)) {
+                    hammingDistance += 1;
                 }
             }
-        } catch (Exception e) {
-            throw new UnsupportedOperationException("Err " + e);
         }
-        
     }
 
     public int getHammingDistance() {
