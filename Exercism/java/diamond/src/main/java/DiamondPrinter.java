a47a74794f925f110af89d8bfa084b2395316aac
MustafaJ4m <mustafaj4m@gmail.com>
2020-05-06 20:58:49 +0200

    Diamond solved

--- Exercism/java/diamond/src/main/java/DiamondPrinter.java
+++ Exercism/java/diamond/src/main/java/DiamondPrinter.java
@@ -1,9 +1,25 @@
+import java.util.ArrayList;
 import java.util.List;
 
 class DiamondPrinter {
 
     List<String> printToList(char a) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        List<String> lines = new ArrayList<>();
+        int diffFromA = a - 'A';
+        lines.add(getPadding(diffFromA) + 'A' + getPadding(diffFromA));
+        for (int i = 1; i <= diffFromA; i++) {
+            int innerPadding = 2 * i - 1;
+            int outerPadding = diffFromA - i;
+            lines.add(getPadding(outerPadding) + (char) ('A' + i) +
+                    getPadding(innerPadding) + (char) ('A' + i) + getPadding(outerPadding));
+        }
+        for (int i = lines.size() - 2; i >= 0; i--) {
+            lines.add(lines.get(i));
+        }
+        return lines;
     }
 
+    private String getPadding(int spaceCount) {
+        return " ".repeat(spaceCount);
+    }
 }
