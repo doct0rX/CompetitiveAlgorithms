f57191252a169170ca2279969023aaaac0625ade
MustafaJ4m <mustafaj4m@gmail.com>
2020-05-07 19:41:06 +0200

    beer-song submission

--- Exercism/java/beer-song/src/main/java/BeerSong.java
+++ Exercism/java/beer-song/src/main/java/BeerSong.java
@@ -1,45 +1,26 @@
 class BeerSong {
 
-    BeerSong() {
-    }
-
-    String sing(int bottles, int repeats) {
-        StringBuilder stringBuilder = new StringBuilder();
-        while (bottles > 0 && repeats > 0) {
-            if (bottles == 1 && repeats == 1) {
-                stringBuilder.append("1 bottle of beer on the wall, 1 bottle of beer.\n" +
-                        "Take it down and pass it around, no more bottles of beer on the wall.\n\n");
-                break;
+    public String sing(int bottles, int repeats) {   // Bottles describe starting line (starting point) && Repeats describe number of lines (ending point)
+        StringBuilder result = new StringBuilder();
+        String basicLine = "%s bottles of beer on the wall, %s bottles of beer.\n" +
+                "Take one down and pass it around, %s bottles of beer on the wall.\n\n";
+        for (int i = bottles; i > bottles - repeats; i--) {
+            if (i-1 > 1) {
+                result.append(String.format(basicLine, i, i, i - 1));
+            } else if(i - 1 == 1) {
+                result.append(String.format("%s bottles of beer on the wall, %s bottles of beer.\n" +
+                        "Take one down and pass it around, %s bottle of beer on the wall.\n\n", i, i, i - 1));
+            } else if(i - 1 == 0) {
+                result.append(String.format("%s bottle of beer on the wall, %s bottle of beer.\n" +
+                        "Take it down and pass it around, no more bottles of beer on the wall.\n\n", i, i));
+            } else {
+                result.append("No more bottles of beer on the wall, no more bottles of beer.\n" + "Go to the store and buy some more, 99 bottles of beer on the wall.\n\n");
             }
-            stringBuilder.append(takeOneDownAndPassAround(bottles, repeats));
-            bottles--; repeats--;
         }
-        if (bottles == 0 && repeats > 0) {
-            stringBuilder.append(takeOneDownAndPassAround(bottles, repeats));
-        }
-        return stringBuilder.toString();
+        return result.toString();
     }
 
-    String singSong() {
+    public String singSong(){
         return sing(99, 100);
-    }
-
-    String takeOneDownAndPassAround(int nBottles, int nRepeats) {
-        if (nBottles > 2) {
-            return nBottles + " bottles of beer on the wall, " + nBottles + " bottles of beer.\n" +
-                    "Take one down and pass it around, " + (nBottles - 1) + " bottles of beer on the wall.\n\n";
-        } else if (nBottles < nRepeats && nBottles == 1) {
-            return "1 bottle of beer on the wall, 1 bottle of beer.\n" +
-                    "Take it down and pass it around, no more bottles of beer on the wall.\n\n";
-        } else if (nBottles > 1) {
-            return "2 bottles of beer on the wall, 2 bottles of beer.\n" +
-            "Take one down and pass it around, 1 bottle of beer on the wall.\n\n";
-        } else if (nBottles == 1) {
-            return "Take one down and pass it around, 1 bottle of beer on the wall.\n\n" +
-            "Take it down and pass it around, no more bottles of beer on the wall.\n\n";
-        } else {
-            return "No more bottles of beer on the wall, no more bottles of beer.\n" +
-                    "Go to the store and buy some more, 99 bottles of beer on the wall.\n\n";
-        }
     }
 }
