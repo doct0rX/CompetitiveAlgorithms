fa4b563c5107025013e0e455c3591cedbc834c2f
MustafaJ4m <mustafaj4m@gmail.com>
2020-05-08 16:19:37 +0200

    Scrabble using streams solved

--- Exercism/java/scrabble-score/src/main/java/ScrabbleStreams.java
+++ Exercism/java/scrabble-score/src/main/java/ScrabbleStreams.java
@@ -1,11 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class ScrabbleStreams {
 
+    /**
+     * here setting every char w/ its integer key to the map and points it to integer value;
+     */
+
+    String word;
+    static final Map<Integer, Integer> lScore = new HashMap<>(26);
+    static {
+        "AEIOULNRST".chars().forEach(c -> lScore.put(c, 1));
+        "DG".chars().forEach(c -> lScore.put(c, 2));
+        "BCMP".chars().forEach(c -> lScore.put(c, 3));
+        "FHVWY".chars().forEach(c -> lScore.put(c, 4));
+        "K".chars().forEach(c -> lScore.put(c, 5));
+        "JX".chars().forEach(c -> lScore.put(c, 8));
+        "QZ".chars().forEach(c -> lScore.put(c, 10));
+    }
+
     ScrabbleStreams(String word) {
-        
+        this.word = word.toUpperCase();
     }
 
     int getScore() {
-
-        return 0;
+        return word.chars().reduce(0, (a, b) -> a + lScore.get(b));
     }
 }
