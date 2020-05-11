d961fec5b6d6cbddd07e645714e22d55a1927a8a
MustafaJ4m <mustafaj4m@gmail.com>
2020-05-11 14:50:52 +0200

    secret-handshake solved

--- Exercism/java/secret-handshake/src/main/java/HandshakeCalculator.java
+++ Exercism/java/secret-handshake/src/main/java/HandshakeCalculator.java
@@ -1,9 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 class HandshakeCalculator {
 
     List<Signal> calculateHandshake(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+        List<Signal> hs = new ArrayList<Signal>();
+        boolean reverse = false;
+        String binaryString = Integer.toBinaryString(number);
+        System.out.println(binaryString);
+        int binaryInt = Integer.parseInt(binaryString);
+        while (binaryInt >= 10000) {
+            reverse = !reverse;
+            binaryInt -= 10000;
+        }
+        while (binaryInt >= 1000) {
+            hs.add(0, Signal.JUMP);
+            binaryInt -= 1000;
+        }
+        while (binaryInt >= 100) {
+            hs.add(0, Signal.CLOSE_YOUR_EYES);
+            binaryInt -= 100;
+        }
+        while (binaryInt >= 10) {
+            hs.add(0, Signal.DOUBLE_BLINK);
+            binaryInt -= 10;
+        }
+        while (binaryInt >= 1) {
+            hs.add(0, Signal.WINK);
+            binaryInt -= 1;
+        }
+        if (reverse) { Collections.reverse(hs); }
 
+        return hs;
+    }
 }
