1226d94be389f3dc2b3d0f37e6fe47b723780f9b
MustafaJ4m <mustafaj4m@gmail.com>
2020-05-22 22:58:40 +0200

    matrix solved

--- Exercism/java/matrix/src/main/java/Matrix.java
+++ Exercism/java/matrix/src/main/java/Matrix.java
@@ -1,15 +1,37 @@
-
 class Matrix {
 
+    private final String matrix;
+    String[] matrixArr;
+
     Matrix(String matrixAsString) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.matrix = matrixAsString;
+        this.matrixArr = matrixAsString.split("\n");
     }
 
     int[] getRow(int rowNumber) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int[] res = new int[getColsNum()];
+        String[] intsStr = matrixArr[rowNumber - 1].split(" ");
+        for (int i = 0; i < getColsNum(); i++) {
+            res[i] = Integer.parseInt(intsStr[i]);
+        }
+        return res;
     }
 
     int[] getColumn(int columnNumber) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int[] res = new int[getRowsNum()];
+
+        for (int i = 0; i < getRowsNum(); i++) {
+            String[] cols = matrixArr[i].split(" ");
+            res[i] = Integer.parseInt(cols[columnNumber - 1]);
+        }
+        return res;
+    }
+
+    int getRowsNum() {
+        return matrixArr.length;
+    }
+
+    int getColsNum() {
+        return matrixArr[0].split(" ").length;
     }
 }
