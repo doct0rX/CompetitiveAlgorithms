5cd118c33fedee79e31582e095238de74325a0f3
MustafaJ4m <mustafaj4m@gmail.com>
2021-04-17 13:33:15 +0200

    sadle points enhanced solution

--- Exercism/java/saddle-points/src/main/java/Matrix.java
+++ Exercism/java/saddle-points/src/main/java/Matrix.java
@@ -12,10 +12,9 @@ class Matrix {
 
     Set<MatrixCoordinate> getSaddlePoints() {
         HashSet<MatrixCoordinate> res = new HashSet<MatrixCoordinate>();
-        int currentPosValue;
         for (int i = 0; i < pValues.size(); i++) {
             for (int j = 0; j < pValues.get(i).size(); j++) {
-                currentPosValue = pValues.get(i).get(j);
+                int currentPosValue = pValues.get(i).get(j);
                 if (checkRows(currentPosValue, i, j) && checkCols(currentPosValue, i, j)) {
                     res.add(new MatrixCoordinate(i + 1, j + 1));
                 }
@@ -26,26 +25,20 @@ class Matrix {
 
 
     boolean checkRows(int rowVal, int xCoordination, int yCoordination) {
-        int totalVals = 0;
         if (pValues.get(xCoordination).size() > 0) {
             for (int i = 0; i < pValues.get(xCoordination).size(); i++) {
-                if (rowVal >= pValues.get(xCoordination).get(i)) {
-                    ++totalVals;
-                }
+                if (rowVal < pValues.get(xCoordination).get(i)) { return false; }
             }
         }
-        return totalVals == pValues.get(xCoordination).size();
+        return true;
     }
 
     boolean checkCols(int colVal, int xCoordination, int yCoordination) {
-        int totalVals = 0;
         if (pValues.size() > 0) {
             for (int i = 0; i < pValues.size(); i++) {
-                if (colVal <= pValues.get(i).get(yCoordination)) {
-                    ++totalVals;
-                }
+                if (colVal > pValues.get(i).get(yCoordination)) { return false; }
             }
         }
-        return totalVals == pValues.size();
+        return true;
     }
 }
