c689221d3cbcf2b995158ac1df739dd754576b87
MustafaJ4m <mustafaj4m@gmail.com>
2021-04-29 13:48:13 +0200

    word-count partially solved

--- Exercism/java/word-count/src/main/java/WordCount.java
+++ Exercism/java/word-count/src/main/java/WordCount.java
@@ -1,10 +1,31 @@
-/*
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+class WordCount {
 
-Please remove this comment when submitting your solution.
+    public WordCount() {
 
-*/
+    }
+
+    Map<String, Integer> phrase(String words) {
+        final Pattern splitWords = Pattern.compile("\\W+");
+//        return splitWords.splitAsStream(words.toLowerCase())
+//                .collect(Collectors.groupingBy(Function.identity(),
+//                        Collectors.collectingAndThen(Collectors.counting(), Long::intValue)));
+
+        String wordsClean = splitWords.matcher(words).replaceAll(" ")
+                .trim().toLowerCase().replaceAll(" '", ""). replaceAll("' ", "");
+        Map<String, Integer> res = new HashMap<String, Integer>();
+        for (String word: wordsClean.split("(\\s+)")) {
+            if (res.containsKey(word)) {
+                res.replace(word, res.get(word) + 1);
+            } else {
+                res.put(word, 1);
+            }
+        }
+        return res;
+    }
+}
