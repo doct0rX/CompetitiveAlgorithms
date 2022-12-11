7ab7bb6d6372d06e82822450414188387720c28d
Mustafa <mustafaj4m@gmail.com>
2022-12-12 01:58:37 +0200

    Update MaxPoints.java

--- LeetCode/randomAlgorithms/MaxPoints.java
+++ LeetCode/randomAlgorithms/MaxPoints.java
@@ -1,29 +1,24 @@
 // problem #1423
 
 class Solution {
+
     public int maxScore(int[] cardPoints, int k) {
-        int frontScore = 0;
-        int rearScore = 0;
-        int n = cardPoints.length;
+        int len = cardPoints.length;
+        int frontScore, rearScore, maxScore;
 
-        for (int i = 0; i < k; i++) {
-            frontScore += cardPoints[i];
-        }
+        frontScore = Arrays.stream(cardPoints, 0 , k).sum();
+        maxScore = frontScore;
+        rearScore = 0;
 
-        // take all k cards from the beginning
-        int maxScore = frontScore;
-
-        // take i from the beginning and k - i from the end
         for (int i = k - 1; i >= 0; i--) {
-            rearScore += cardPoints[n - (k - i)];
+            rearScore += cardPoints[len - k + i];
             frontScore -= cardPoints[i];
-            int currentScore = rearScore + frontScore;
-            maxScore = Math.max(maxScore, currentScore);
+            maxScore = Math.max(frontScore + rearScore, maxScore);
         }
-
         return maxScore;
     }
 }
+
 
 // time complexity: O(2.k) => O(k)
 // space complexity: O(1)
